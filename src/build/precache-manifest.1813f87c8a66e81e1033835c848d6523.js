ccc96fea927b14a957b998b235fba3eb957d49ad
Yosef-Ali <yosethio@yahoo.com>
2020-08-25 09:33:41 +0300

    first commit

---
+++ src/build/precache-manifest.1813f87c8a66e81e1033835c848d6523.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "175bcac53347e085d75e",
+    "url": "/static/css/main.d4589106.chunk.css"
+  },
+  {
+    "revision": "175bcac53347e085d75e",
+    "url": "/static/js/main.6dc74309.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4f0bb97516cdfcfc2617",
+    "url": "/static/js/2.470c326c.chunk.js"
+  },
+  {
+    "revision": "3ccf5c50d9052520a38f1daf1366c8d2",
+    "url": "/index.html"
+  }
+];
